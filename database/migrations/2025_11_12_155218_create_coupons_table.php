8c2b48b4271ce607cd1aeb9d5cf2ba385acf4430
uswa <uswainam267@gmail.com>
2025-11-13 10:00:05 +0500

    all files

---
+++ database/migrations/2025_11_12_155218_create_coupons_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('coupons', function (Blueprint $table) {
+            $table->id();
+            $table->string('code')->unique();
+            $table->enum('discount_type', ['percentage', 'fixed']);
+            $table->decimal('discount_value', 8, 2);
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->decimal('min_order_amount', 8, 2)->nullable();
+            $table->integer('max_uses')->nullable();
+            $table->boolean('status')->default(true); // true for active, false for inactive
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('coupons');
+    }
+};
---
+++ database/migrations/2025_11_12_155431_create_reviews_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('product_id');
+            $table->tinyInteger('rating'); // e.g., 1 to 5
+            $table->text('comment')->nullable();
+            $table->enum('status', ['approved', 'pending'])->default('pending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
