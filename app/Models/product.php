f2da1042c04a5f448cce715f3e7af736575d179e
uswa <uswainam267@gmail.com>
2025-11-18 12:16:46 +0500

    added casts for product

--- app/Models/product.php
+++ app/Models/product.php
@@ -4,20 +4,40 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class product extends Model
+class Product extends Model
 {
     protected $fillable = [
-    'category_id',
-    'sub_category_id',
-    'name',
-    'slug',
-    'sku',
-    'price',
-    'discount_price',
-    'stock',
-    'short_description',
-    'long_description',
-    'status', // 'active' / 'inactive'
-    'featured', // boolean
-];
+        'category_id',
+        'sub_category_id',
+        'name',
+        'slug',
+        'sku',
+        'price',
+        'discount_price',
+        'stock',
+        'short_description',
+        'long_description',
+        'status',
+        'featured',
+    ];
+
+    protected $casts = [
+        'category_id'     => 'integer',
+        'sub_category_id' => 'integer',
+
+        'price'           => 'decimal:2',
+        'discount_price'  => 'decimal:2',
+
+        'stock'           => 'integer',
+
+        'name'            => 'string',
+        'slug'            => 'string',
+        'sku'             => 'string',
+
+        'short_description' => 'string',
+        'long_description'  => 'string',
+
+        'status'          => 'boolean',   // active/inactive
+        'featured'        => 'boolean',   // 1 or 0
+    ];
 }
