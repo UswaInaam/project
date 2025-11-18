450e2e943a8b3c2a836687534fc402bfe115345f
uswa <uswainam267@gmail.com>
2025-11-18 12:18:40 +0500

    added casts for productVarient

--- app/Models/ProductVariant.php
+++ app/Models/ProductVariant.php
@@ -7,10 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 class ProductVariant extends Model
 {
     protected $fillable = [
-    'product_id',
-    'attribute', // e.g., 'Size'
-    'value',     // e.g., 'Large'
-    'price_adjustment',
-    'stock',
-];
+        'product_id',
+        'attribute',        // e.g., 'Size'
+        'value',            // e.g., 'Large'
+        'price_adjustment', // numeric adjustment
+        'stock',
+    ];
+
+    protected $casts = [
+        'product_id'       => 'integer',
+        'stock'            => 'integer',
+
+        'attribute'        => 'string',
+        'value'            => 'string',
+        'price_adjustment' => 'decimal:2',
+    ];
 }
