7cd1f801b2f98321b4c2e69242a2cac53d6c823d
uswa <uswainam267@gmail.com>
2025-11-18 12:08:26 +0500

    added file

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -7,14 +7,28 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     protected $fillable = [
-    'user_id',
-    'order_number',
-    'total_amount',
-    'discount',
-    'shipping_cost',
-    'payment_status', // 'pending', 'paid', 'failed'
-    'order_status',   // 'pending', 'processing', 'completed', 'cancelled'
-    'shipping_address_id',
-    'payment_method', // 'cash_on_delivery', 'stripe', 'paypal', etc.
-];
+        'user_id',
+        'order_number',
+        'total_amount',
+        'discount',
+        'shipping_cost',
+        'payment_status',
+        'order_status',
+        'shipping_address_id',
+        'payment_method',
+    ];
+
+    protected $casts = [
+        'user_id' => 'integer',
+        'shipping_address_id' => 'integer',
+
+        'total_amount' => 'decimal:2',
+        'discount' => 'decimal:2',
+        'shipping_cost' => 'decimal:2',
+
+        'payment_status' => 'string',
+        'order_status' => 'string',
+        'order_number' => 'string',
+        'payment_method' => 'string',
+    ];
 }
